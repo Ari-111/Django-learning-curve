edd828c03c15824829b0c6069af2f83d6f4edb96
Ari-111 <ashishkmr8800@gamil.com>
2025-02-12 12:43:40 +0530

    updated admin panel and run tests

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import Question
+from .models import Question,Choice
 
-admin.site.register(Question)  # Makes Question model visible in admin panel
+admin.site.site_header = "Polls management Admin"
+
+
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+    class QuestionAdmin(admin.ModelAdmin):
+        fieldsets = [
+            (None, {'fields': ['question_text']}),
+            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ]
+        
+        list_display = ('question_text', 'pub_date', 'was_published_recently')
+        list_filter = ['pub_date']
+        search_fields = ['question_text']
+inlines = [ChoiceInline]
+
+
+admin.site.register(Question)
+
+
+
+  # Makes Question model visible in admin panel
 
 
 # Register your models here.
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from .models import Question
+from django.urls import reverse
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+        def test_no_questions(self):
+            """If no questions exist, an appropriate message is displayed."""
+            response = self.client.get(reverse('polls:index'))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "No polls are available.")
+            self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
 # Create your tests here.
